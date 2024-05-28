852bcf3c735a124febdb5d0fbc9e1badbfaaf38c
marsavil <msvillaescusa@gmail.com>
2024-05-28 17:58:04 -0300

    Login session

--- src/controllers/sesion.ts
+++ src/controllers/sesion.ts
@@ -9,8 +9,8 @@ const sesion = {
   login : async function (req: Request, res: Response) {
     try {
       console.log("entró")
-      const { dni, password, name } = req.body;
-      if ( !dni && !name ) return res.status(400).send("Debe identificarse para poder proseguir")
+      const { dni, password, surname } = req.body;
+      if ( !dni && !surname ) return res.status(400).send("Debe identificarse para poder proseguir")
       if( !password ) return res.status(400).send("Debe ingresar una contraseña")
       if ( dni ) {
         const medico = await Medico.find({ dni });
@@ -18,14 +18,14 @@ const sesion = {
         if ( medico.length ) {
           const passwordMatch = await bcrypt.compare(password, medico[0].password)
           if( passwordMatch ) {
-            return res.status(200).send({message: `Usted se encuentra loggeado como ${medico[0].name} ${medico[0].surname}`, data: medico[0]})
+            return res.status(200).send({message: `Usted se encuentra loggeado como ${medico[0].surname} ${medico[0].surname}`, data: medico[0]})
           }else {
             return res.status(400).send("Contraseña de médico incorrecta")
           }
         } else if ( empleado.length ) {
           const passwordMatch = await bcrypt.compare(password, empleado[0].password)
           if ( passwordMatch ) {
-            return res.status(200).send({message: `Usted se encuentra loggeado como ${empleado[0].name} ${empleado[0].surname}`, data: empleado[0]})
+            return res.status(200).send({message: `Usted se encuentra loggeado como ${empleado[0].surname} ${empleado[0].surname}`, data: empleado[0]})
           } else {
             return res.status(400).send("Contraseña de empleado incorrecta")
           }
@@ -33,8 +33,8 @@ const sesion = {
           return res.status(400).send("Verifique los datos ingresados")
         }
       }
-      if ( name ) {
-        const admin = await Admin.find({ name })
+      if ( surname ) {
+        const admin = await Admin.find({ surname })
         if( admin.length ){
           const passwordMatch = await bcrypt.compare(password, admin[0].password)
           if ( passwordMatch ) {
